c6590a51ab4cee50b38822868f27e741892b1ae8
Didaka19 <124637285+Didaka19@users.noreply.github.com>
2023-03-23 19:04:12 +0000

    Add files via upload

---
+++ pa-chat-room-main/client/src/main/java/Main.java
@@ -0,0 +1,23 @@
+package client.src.main.java;
+
+
+
+public class Main {
+
+    public static void main ( String[] args ) {
+
+
+
+       ClientThread client = new ClientThread ( 8888 , 1);
+       client.start();
+
+       // ClientThread client2 = new ClientThread ( 8888 , 2);
+        //client2.start ( );
+
+       // ClientThread client3 = new ClientThread ( 8888 , 3);
+       // client3.start ( );
+
+        //ClientThread client4 = new ClientThread ( 8888 , 4);
+        //client4.start ( );
+    }
+}
